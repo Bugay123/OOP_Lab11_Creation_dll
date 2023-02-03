e91db03d9d463bfb6f507704fa52785714d108d9
Alex Bugay <bosco.bugai@gmail.com>
2023-02-03 12:13:12 +0200

    creation dll

---
+++ ConsoleTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleTask1;
+class Program
+{
+    /// <summary>
+    /// Використання методів Gamma() та Beta() з бібліотеки MyDll
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        double x = 1.825;
+        double y = 18.225;
+        double z = -3.298;
+
+        double myGamma = Task1.MyDll.Gamma(x, y, z);
+        Console.WriteLine(myGamma);
+        double myBeta = Task1.MyDll.Beta(x, y, z);
+        Console.WriteLine(myBeta);
+    }
+}
+
---
+++ Task1/MyDll.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1;
+public class MyDll
+{
+    /// <summary>
+    /// Gamma(x,y,z)
+    /// </summary>
+    /// <param name="x" double></param>
+    /// <param name="y" double></param>
+    /// <param name="z" double></param>
+    /// <returns>Повертає значення виразу |x^y/x - cbrt(y/x)|</returns>
+    ///
+
+    public static double Gamma(double x, double y, double z)
+    {
+        double gamma = Math.Abs(Math.Pow(x, (y / x)) - Math.Cbrt(y / x));
+        return gamma;
+    }
+
+    /// <summary>
+    /// Beta
+    /// </summary>
+    /// <param name="x" double></param>
+    /// <param name="y" double></param>
+    /// <param name="z" double></param>
+    /// <returns>Повертає значення виразу (y-z)*((y-z)/(y-x))/1+ (y-x)^2</returns>
+    public static double Beta(double x, double y, double z)
+    {
+        double beta = (y - x) * ((y - z / (y - x)) / (1 + Math.Pow((y - z), 2)));
+        return beta;
+    }
+}
